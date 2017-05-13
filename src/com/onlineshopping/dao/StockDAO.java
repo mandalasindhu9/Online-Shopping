022a65b9ccd09ddca9f91d454b142719c4033479
mandalasindhu9 <mandalasindhu9@gmail.com>
2017-05-12 23:18:34 -0600

    Commiting JSP Files

--- src/com/onlineshopping/dao/StockDAO.java
+++ src/com/onlineshopping/dao/StockDAO.java
@@ -1,7 +1,20 @@
 package com.onlineshopping.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
+import org.hibernate.SessionFactory;
+import org.onlineshopping.model.*;
 
+@Repository
 public class StockDAO extends HibernateDaoSupport {
-
+	
+	@Autowired
+	private SessionFactory session;
+	
+	public void updateStock(ComponentDetails componentDetails){
+		
+		session.getCurrentSession().update(componentDetails);
+		
+	}
 }
--- src/com/onlineshopping/dao/TransactionDAO.java
+++ src/com/onlineshopping/dao/TransactionDAO.java
@@ -1,7 +1,43 @@
 package com.onlineshopping.dao;
 
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
 
+import org.onlineshopping.model.*;
+
+@Repository
 public class TransactionDAO extends HibernateDaoSupport {
+	
+	@Autowired
+	private SessionFactory session;
+	
+	public List<ProductDetails> productDetails(ProductDetails product) {
+	
+		List<ProductDetails> productDetails = session.getCurrentSession().createQuery("from product where product_id=" + product.getProduct_id()).list();
+		
+		for(ProductDetails p:productDetails){
+			System.out.println("Details: "+p);
+		}
+		return productDetails;	
+	}
+	
+	public void addShipmentInformation(ShipmentOrderDetails ship){
+		
+		session.getCurrentSession().saveOrUpdate(ship);
+		
+	}
+	
+	public void addOrderDetails(OrderProductDetails order){
+		
+		session.getCurrentSession().saveOrUpdate(order);
+	}
 
+	public void addCardDetails(PaymentMethodDetails payDetails){
+		
+		session.getCurrentSession().saveOrUpdate(payDetails);
+	}
 }
